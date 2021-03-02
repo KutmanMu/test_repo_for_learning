1b7b02fec8499238cee66f84ebb142a5b31d188e
zedd_zedd <kuka.bishkek@gmail.com>
2021-03-02 17:58:24 +0600

    gitignore and , .cs files are added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructures
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Player player = new Player();
+            Enemy enemy = new Enemy();
+
+            Console.WriteLine("Player : The value of Attack : {0}" , player.GetAttack());
+            Console.WriteLine("Enemy : The value of Attack : {0}" , enemy.GetAttack());
+
+            Console.ReadKey();
+
+
+        }
+
+    }
+}
